ade674738a24c8e6957a49f06f247b15c4a11a46
Cindy5588 <102703596+Cindy5588@users.noreply.github.com>
2022-04-14 19:51:38 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,18 @@
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+def getBondDuration(y, face, couponRate,m, ppy = 1):
+    pvcftsum = 0
+    pvcfsum = 0
+    cf = face/ppy*couponRate
+  
+    for t in range(1,m+1):   
+        pvcf = cf*(1+y)**(-t)
+        pvcfsum += pvcf
+        pvcft = pvcf*t
+        pvcftsum += pvcft
+  
+    pvcfsum = pvcfsum + face*(1+y)**-m/ppy
+    pvcftsum = pvcftsum +  m*face*(1+y)**-m/ppy
+    
+    BondDuration = pvcftsum/pvcfsum
+    
+    return(BondDuration)
