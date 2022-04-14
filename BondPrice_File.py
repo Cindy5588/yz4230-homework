810569ab14175903fc33189ac1a24dcd1a01bc5a
Cindy5588 <102703596+Cindy5588@users.noreply.github.com>
2022-04-14 19:54:17 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,18 @@
 
+y = 0.03
+face = 2000000
+couponRate = 0.04
+m = 10
+ppy = 1
+ppy = 2
 
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+def getBondPrice(y, face, couponRate, m, ppy = 1):
+    pvcfsum = 0
+    for t in range(1,m*ppy+1):
+        cf = couponRate*face/ppy
+        pv = (1+y/ppy)**(-t)
+        pvcf = pv*cf
+        pvcfsum += pvcf
+        
+    bondprice = pvcfsum + (1+y/ppy)**-(m*ppy)*face
+    return(bondprice)
